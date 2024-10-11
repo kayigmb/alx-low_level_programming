34c2dd155b951b112402e19e61c08f5de0bd7438
kayigmb <kayigm1@gmail.com>
2024-10-11 21:44:17 +0200

    c hash

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,20 +8,20 @@
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new node.
  */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t *new;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n) {
+  dlistint_t *new;
 
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-	if (*head != NULL)
-		(*head)->prev = new;
-	*head = new;
+  new = malloc(sizeof(dlistint_t));
+  if (new == NULL)
+    return (NULL);
 
-	return (new);
+  new->n = n;
+  new->prev = NULL;
+  new->next = *head;
+  if (*head != NULL)
+    (*head)->prev = new;
+  *head = new;
+
+  return (new);
 }
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,29 +8,29 @@
  * Return: If the function fails - NULL.
  *         Otherwise - the address of the new node.
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
-{
-	dlistint_t *new, *last;
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n) {
+  dlistint_t *new, *last;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+  new = malloc(sizeof(dlistint_t));
+  if (new == NULL)
+    return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+  new->n = n;
+  new->next = NULL;
 
-	if (*head == NULL)
-	{
-		new->prev = NULL;
-		*head = new;
-		return (new);
-	}
+  if (*head == NULL) {
+    new->prev = NULL;
+    *head = new;
+    return (new);
+  }
 
-	last = *head;
-	while (last->next != NULL)
-		last = last->next;
-	last->next = new;
-	new->prev = last;
+  last = *head;
 
-	return (new);
+  while (last->next != NULL)
+    last = last->next;
+
+  last->next = new;
+  new->prev = last;
+
+  return (new);
 }
